e2b54a3efd5a4a03122f0b4edd176f1d81c4a974
Martin Scheiber <martin.scheiber@aau.at>
2022-07-19 11:05:30 +0200

    add: added catkin_make to docker test
    Closes #15.

--- docker/docker_application_test.sh
+++ docker/docker_application_test.sh
@@ -1,8 +1,20 @@
 #!/bin/bash -e
 echo "Copy Source Files..."
 cp -r /source/ /catkin_ws/src/mars_ros
-echo "Building the Appliaction..."
+
+# build with catkin build
+echo "Building the Appliaction with 'catkin build' ..."
 cd /catkin_ws
 catkin build -DCMAKE_BUILD_TYPE=Release
 catkin build -DCMAKE_BUILD_TYPE=Debug
+echo "Building the Appliaction with 'catkin build' - DONE"
 
+# reset workspace and build again with catkin_make for full portability
+catkin clean -y
+rm -rf .catkin_tools
+
+# build with catkin_make
+echo "Building the Appliaction with 'catkin_make' ..."
+catkin_make -DCMAKE_BUILD_TYPE=Release
+catkin_make -DCMAKE_BUILD_TYPE=Debug
+echo "Building the Appliaction with 'catkin_make' - DONE"
